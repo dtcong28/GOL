a64b297c45d6138373ce263778fb5384d48a6d21
dtcong28 <dotiencong28082001@gmail.com>
2022-08-08 13:58:53 +0700

    code optimization in role_controller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -32,25 +32,17 @@ class RoleController extends Controller
 
     public function store(RoleRequest $request)
     {
-        $success = false;
         DB::beginTransaction();
 
         try {
             $role = $this->roleRepository->save($request->validated());
-            if ($role) {
-                $permission_ids = $request->input('permission_ids');
-                $role->permissions()->sync($permission_ids);
-                $success = true;
-            }
+            $role->permissions()->sync($request->permission_ids);
+            DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
             return redirect()->back()->with('message', 'Something went wrong');
         }
-
-        if ($success) {
-            DB::commit();
-            return redirect()->route('role.index')->with('success', 'Creation success.');
-        }
+        return redirect()->route('role.index')->with('success', 'Creation success.');
     }
 
     public function show($id)
@@ -58,7 +50,7 @@ class RoleController extends Controller
         if (!$role = $this->roleRepository->findById($id)) {
             abort(404);
         }
-        return view('admin.role.form', ['role' => $role,'act'=> 'show','permissionGroups' => $this->permissionGroupRepository->getAll()]);
+        return view('admin.role.form', ['role' => $role, 'act' => 'show', 'permissionGroups' => $this->permissionGroupRepository->getAll()]);
     }
 
     public function edit($id)
@@ -66,35 +58,38 @@ class RoleController extends Controller
         if (!$role = $this->roleRepository->findById($id)) {
             abort(404);
         }
-        return view('admin.role.form', ['role' => $role,'permissionGroups' => $this->permissionGroupRepository->getAll()]);
+        return view('admin.role.form', ['role' => $role, 'permissionGroups' => $this->permissionGroupRepository->getAll()]);
     }
 
     public function update(RoleRequest $request, $id)
     {
-        $success = false;
         DB::beginTransaction();
 
         try {
             $role = $this->roleRepository->save($request->validated(), ['id' => $id]);
-            if ($role) {
-                $permission_ids = $request->input('permission_ids');
-                $role->permissions()->sync($permission_ids);
-                $success = true;
-            }
+            $role->permissions()->sync($request->permission_ids);
+            DB::commit();
         } catch (\Exception $e) {
             DB::rollback();
             return redirect()->back()->with('message', 'Something went wrong');
         }
 
-        if ($success) {
-            DB::commit();
-            return redirect()->route('role.index')->with('success', 'Update success.');
-        }
+        return redirect()->route('role.index')->with('success', 'Update success.');
     }
 
     public function destroy($id)
     {
-        $this->roleRepository->deleteById($id);
-        return redirect()->route('role.index');
+        DB::beginTransaction();
+
+        try {
+            $this->roleRepository->findById($id)->permissions()->detach();
+            $this->roleRepository->deleteById($id);
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->back()->with('message', 'Something went wrong');
+        }
+
+        return redirect()->route('role.index')->with('success', 'Delete success.');
     }
 }
